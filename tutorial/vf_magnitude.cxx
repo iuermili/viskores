9ff982235ce7823b5a1b5f1d7d18069d03588da1
Eric Li <ericli@Erics-MacBook-Pro.local>
2025-06-10 11:24:29 -0400

    implemented CombineToVec2 and ComputeMagnitude worklet

--- tutorial/vf_magnitude.cxx
+++ tutorial/vf_magnitude.cxx
@@ -16,16 +16,41 @@
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
 #include <viskores/cont/Initialize.h>
+#include <viskores/cont/Invoker.h>
 #include <viskores/io/VTKDataSetReader.h>
 #include <viskores/io/VTKDataSetWriter.h>
+#include <viskores/Types.h>
+#include <viskores/worklet/WorkletMapField.h>
+#include <viskores/cont/Field.h>
+#include <viskores/cont/ArrayHandle.h>
+#include <iostream>
+#include <string>
 
-#include <viskores/filter/vector_analysis/VectorMagnitude.h>
-
-// Manual computation of vector magnitude
-/* double ComputeVectorMagnitude(double x, double y, double z)
+// Worklet: combines two scalar fields into a Vec2f field
+struct CombineToVec2: viskores::worklet::WorkletMapField
 {
-    return std::sqrt(x * x + y * y + z * z);
-} */
+    using ControlSignature = void(FieldIn meanX, FieldIn meanY, FieldOut outputVectors);
+
+    VISKORES_EXEC void operator()(viskores::FloatDefault x,
+                                  viskores::FloatDefault y,
+                                  viskores::Vec2f& outVec) const
+    {
+        // Combine x and y 
+        outVec = viskores::Vec2f(static_cast<float>(x), static_cast<float>(y));
+    }
+};
+
+// Worklet: copmutes magnitude of a Vec2f field
+struct ComputeMagnitude: viskores::worklet::WorkletMapField
+{
+    using ControlSignature = void(FieldIn inputVec, FieldOut outputMagnitude);
+
+    VISKORES_EXEC void operator()(const viskores::Vec2f& inVec, viskores::FloatDefault& outMagnitude) const
+    {
+        // Compute magnitude
+        outMagnitude = viskores::Magnitude(inVec);
+    }
+};
 
 int main(int argc, char** argv)
 {
@@ -35,15 +60,30 @@ int main(int argc, char** argv)
     viskores::io::VTKDataSetReader reader("data/uncertainVectorField.vtk");
     viskores::cont::DataSet ds = reader.ReadDataSet();
 
-    // Apply Vector Magnitude filter
-    viskores::filter::vector_analysis::VectorMagnitude filter;
-    filter.SetActiveField("uncertainty");
-    filter.SetOutputFieldName("magnitude");
-    viskores::cont::DataSet output = filter.Execute(ds);
+    // Retrieve meanX and meanY as ArrayHandles
+    auto meanXArray = ds.GetField("meanX").GetData().AsArrayHandle<viskores::cont::ArrayHandle<viskores::FloatDefault>>();
+    auto meanYArray = ds.GetField("meanY").GetData().AsArrayHandle<viskores::cont::ArrayHandle<viskores::FloatDefault>>();
+    
+    // Combine meanX and meanY into a Vec2f vector field 
+    viskores::cont::ArrayHandle<viskores::Vec2f> vectorField;
+    viskores::cont::Invoker invoker;
+    invoker(CombineToVec2{}, meanXArray, meanYArray, vectorField);
+
+    // Add new vector field to dataset
+    viskores::cont::Field vecField("meanXY", ds.GetField("meanX").GetAssociation(), vectorField);
+    ds.AddField(vecField);
+
+    // Compute vector magnitude
+    viskores::cont::ArrayHandle<viskores::FloatDefault> magnitudeValues;
+    invoker(ComputeMagnitude{}, vectorField, magnitudeValues);
+
+    // Add magnitude field to dataset
+    viskores::cont::Field magnitudeField("magnitude", ds.GetField("meanX").GetAssociation(), magnitudeValues);
+    ds.AddField(magnitudeField);
 
     // Write output to file
     viskores::io::VTKDataSetWriter writer("out_vf_magnitude.vtk");
-    writer.WriteDataSet(output);
+    writer.WriteDataSet(ds);
 
     return 0;
 }
