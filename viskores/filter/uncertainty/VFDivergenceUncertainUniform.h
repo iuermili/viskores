1b930535080a3b53d91ba76c930a51b5d6591315
Eric Li <ericli@Erics-MacBook-Pro.local>
2025-06-12 11:48:32 -0400

    reverted filter implementation for now to focus on worklet

--- viskores/filter/uncertainty/VFDivergenceUncertainUniform.h
+++ viskores/filter/uncertainty/VFDivergenceUncertainUniform.h
@@ -15,26 +15,3 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
-
-#ifndef viskores_filter_uncertainty_VFDivergenceUncertainUniform_h
-#define viskores_filter_uncertainty_VFDivergenceUncertainUniform_h
-
-#include <viskores/filter/Filter.h>
-#include <viskores/filter/uncertainty/viskores_filter_uncertainty_export.h>
-
-namespace viskores {
-namespace filter {
-namespace uncertainty {
-
-class VISKORES_FILTER_UNCERTAINTY_EXPORT VFDivergenceUncertainUniform : public viskores::filter::Filter
-{
-public:
-    VISKORES_CONT VFDivergenceUncertainUniform() = default;
-    
-}
-
-} // namespace uncertainty
-} // namespace filter
-} // namespace viskores
-
-#endif
