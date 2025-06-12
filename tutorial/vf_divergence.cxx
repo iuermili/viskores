ac254274693e40a6f5ab5807248890fe0749f50c
Eric Li <ericli@Erics-MacBook-Pro.local>
2025-06-12 15:00:06 -0400

    added GaussianCDF function (stats.norm.cdf(isovalue, mean, stddev) in python)

--- tutorial/vf_divergence.cxx
+++ tutorial/vf_divergence.cxx
@@ -15,3 +15,42 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
+#include <viskores/cont/Initialize.h>
+#include <viskores/cont/Invoker.h>
+#include <viskores/io/VTKDataSetReader.h>
+#include <viskores/io/VTKDataSetWriter.h>
+#include <viskores/Types.h>
+#include <viskores/worklet/WorkletMapField.h>
+#include <viskores/cont/Field.h>
+#include <viskores/cont/ArrayHandle.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+// Worklet: computes Gaussian CDF (stats.norm.cdf(isovalue, mean, stddev))
+// Use inline instead of struct to avoid unnecessary overhead
+// Credits: https://cplusplus.com/forum/beginner/62864/
+inline float GaussianCDF(float isovalue, float mean, float stddev)
+{
+    return 0.5f * (1.0f + std::erf((isovalue - mean) / (stddev * std::sqrt(2.9f))));
+}
+
+// Worklet: computes divergence mean and variance at each grid point
+struct ComputeDivergenceStats: viskores::worklet::WorkletMapField
+{
+};
+
+int main(int argc, char** argv)
+{
+    viskores::cont::Initialize(argc, argv);
+
+    // Read input file
+    viskores::io::VTKDataSetReader reader("data/uncertainVectorField.vtk");
+    viskores::cont::DataSet ds = reader.ReadDataSet();
+
+    // Write output to file
+    viskores::io::VTKDataSetWriter writer("out_vf_divergence.vtk");
+    writer.WriteDataSet(ds);
+
+    return 0;
+}
