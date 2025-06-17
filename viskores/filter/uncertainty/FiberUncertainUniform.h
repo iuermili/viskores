7c9f00c5459075ccf7241d786b9cc7d12b21be43
Eric Li <ericli@Erics-MacBook-Pro.local>
2025-06-17 14:48:36 -0400

    added doxygen comments

--- viskores/filter/uncertainty/FiberUncertainUniform.h
+++ viskores/filter/uncertainty/FiberUncertainUniform.h
@@ -16,12 +16,12 @@
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
 
-//  This code is based on the algorithm presented in the following papers:
+//  This code is based on the algorithm presented in the following paper:
 //  Hari, G., Joshi, N., Wang, Z., Gong, Q., Pugmire, D., Moreland, K.,
 //  Johnson, C. R., Klasky, S., Podhorszki, N., and Athawale, T. M.
-//  (2024). FunM^2C: A Filter for Uncertainty Visualization of MultiVariate Data
-//  on Multi-Core Devices. Oak Ridge National Laboratory (ORNL),
-//  Oak Ridge, TN (United States).
+//  (2024). "FunM2C: A Filter for Uncertainty Visualization of Multivariate Data on Multi-Core Devices," 
+//  in 2024 IEEE Workshop on Uncertainty Visualization: Applications, Techniques, Software, and Decision Frameworks, 
+//  St Pete Beach, FL, USA, 2024, pp. 43-47, doi: 10.1109/UncertaintyVisualization63963.2024.00010. 
 
 #ifndef viskores_filter_uncertainty_FiberUncertainUniform_h
 #define viskores_filter_uncertainty_FiberUncertainUniform_h
@@ -35,15 +35,14 @@ namespace filter
 {
 namespace uncertainty
 {
-/// @brief Visualize fiber surface uncertainty for uniform distributed data.
+/// @brief Visualize fiber uncertainty for uniform distributed data.
 ///
-/// This filter computes the positional uncertainty of fiber surfaces as a
-/// function of uncertainty in bivariate input data, where the data associated with each variable are assumed to
-/// be uniformly distributed and sampled on a regular grid. The uniform
-/// distribution range is given through the input datasets via the minimum
-/// and maximum ensemble X and Ys. Given the uniform distribution range, the computed
-/// fiber surface uncertainty corresponds to uncertainty in bivariate cases in
-/// the fiber uncertainty visualization algorithm.
+/// This filter computes the positional probability of fibers (bivariate features) as a
+/// function of uncertainty in the input bivariate data (with variables denoted by X and Y). 
+/// The data are sampled on a regular grid, and the uncertainty in the data for each variable 
+/// is assumed to be uniformly distributed. The uniform distribution range is estimated from 
+/// the input datasets via the minimum and maximum values of variables X and Y observed across 
+/// the ensemble. 
 ///
 class VISKORES_FILTER_UNCERTAINTY_EXPORT FiberUncertainUniform : public viskores::filter::Filter
 {
@@ -51,16 +50,16 @@ public:
   /// @brief Constructor
   VISKORES_CONT FiberUncertainUniform() = default;
 
-  /// @brief Sets minimum axis.
-  /// Sets minimum value of uniform distribution at each grid point.
+  /// @brief Sets the trait rectangle's bottom left corner.
+  /// Sets the trait rectangle's bottom left corner.
   VISKORES_CONT void SetMinAxis(
     const viskores::Pair<viskores::FloatDefault, viskores::FloatDefault>& minCoordinate)
   {
     this->minAxis = minCoordinate;
   }
 
-  /// @brief Sets maximum axis.
-  /// Sets minimum value of uniform distrubition at each grid point.
+  /// @brief  Sets the trait rectangle's top right corner
+  /// Sets the trait rectangle's top right corner.
   VISKORES_CONT void SetMaxAxis(
     const viskores::Pair<viskores::FloatDefault, viskores::FloatDefault>& maxCoordinate)
   {
@@ -68,42 +67,42 @@ public:
   }
 
   /// @brief Sets minimum X.
-  /// Sets minimum value for trait rectangle's bottom-left corner.
+  /// Sets minimum value of the uniform distribution of the variable X at each grid point.
   VISKORES_CONT void SetMinX(const std::string& fieldName)
   {
     this->SetActiveField(0, fieldName, viskores::cont::Field::Association::Points);
   }
 
-  /// @brief Sets maxmimum Y.
-  /// Sets maxmimum value for trait rectangle's bottom right corner.
+  /// @brief Sets maxmimum X.
+  /// Sets maximum value of the uniform distribution of the variable X at each grid point.
   VISKORES_CONT void SetMaxX(const std::string& fieldName)
   {
     this->SetActiveField(1, fieldName, viskores::cont::Field::Association::Points);
   }
 
   /// @brief Sets minimum Y.
-  /// Sets minimum value for trait rectangle's top left corner.
+  /// Sets minimum value of the uniform distribution of the variable Y at each grid point.
   VISKORES_CONT void SetMinY(const std::string& fieldName)
   {
     this->SetActiveField(2, fieldName, viskores::cont::Field::Association::Points);
   }
 
   /// @brief Sets maxmimum Y.
-  /// Sets maxmimum value for trait rectangle's top right corner.
+  /// Sets maximum value of the uniform distribution of the variable Y at each grid point.
   VISKORES_CONT void SetMaxY(const std::string& fieldName)
   {
     this->SetActiveField(3, fieldName, viskores::cont::Field::Association::Points);
   }
 
   /// @brief Sets number of samples.
-  /// Sets how many samples will be used when computing uncertainty via Monte Carlo.
+  /// Sets how many samples will be used when computing fiber uncertainty using the Monte Carlo approach.
   VISKORES_CONT void SetNumSamples(const viskores::Id& numSamples)
   {
     this->NumSamples = numSamples;
   }
 
-  /// @brief Sets the approach for computing uncertainty.
-  ///  Sets the formula for the corresponding filter that was selected.
+  /// @brief Sets the approach for computing uncertainty (ClosedForm or  MonteCarlo).
+  ///  Sets the approach for the corresponding filter that was selected.
   VISKORES_CONT void SetApproach(const std::string& approach)
   {
     this->Approach = approach;
@@ -112,7 +111,7 @@ public:
 private:
   viskores::Pair<viskores::FloatDefault, viskores::FloatDefault> minAxis;
   viskores::Pair<viskores::FloatDefault, viskores::FloatDefault> maxAxis;
-  std::string Approach = "ClosedForm"; // MonteCarlo, ClosedForm, Mean, Truth
+  std::string Approach = "ClosedForm";
   viskores::Id NumSamples = 5000;
 
   VISKORES_CONT viskores::cont::DataSet DoExecute(
